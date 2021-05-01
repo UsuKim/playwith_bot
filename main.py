5f5cdc2617bd9f3973de63f032d283c330412e55
김 현우 <a01046807116@gmail.com>
2021-05-01 17:11:10 +0900

    asdf

--- main.py
+++ main.py
@@ -128,12 +128,12 @@ async def change_price():
     doge = []
     time = []
     for i in range(len(data)):
-        btc.append(data[i][1] // 100)
-        eth.append(data[i][2] // 10)
-        ltc.append(data[i][3])
-        dot.append(data[i][4] * 10)
-        ada.append(data[i][5] * 100)
-        doge.append(data[i][6] * 1000)
+        btc.append(data[i][1]**0.4)
+        eth.append(data[i][2]**0.47)
+        ltc.append(data[i][3]**0.53)
+        dot.append(data[i][4]**0.61)
+        ada.append(data[i][5]**0.84)
+        doge.append(data[i][6])
         time.append(i*3/60)
     matplotlib.rcParams['axes.unicode_minus'] = False
     matplotlib.rcParams['font.family'] = "NanumGothicCoding"
