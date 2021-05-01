cff86092fa49f026a6433dc873d43cc95cfe4dc9
김 현우 <a01046807116@gmail.com>
2021-05-01 21:09:53 +0900

    asdf

--- main.py
+++ main.py
@@ -137,7 +137,8 @@ async def change_price():
         time.append(i*3/60)
     matplotlib.rcParams['axes.unicode_minus'] = False
     matplotlib.rcParams['font.family'] = "NanumGothicCoding"
-    fig = plt.figure().figsize([15,15])
+    matplotlib.rcParams['figure.figsize'] = (6, 3)
+    fig = plt.figure()
     ax1 = fig.add_subplot(111)
     a = ax1.plot(time, btc, color='darkorange', label="비트코인")
     ax1.set_ylim([min(btc)-min(btc)/500, max(btc)+max(btc)/500])
