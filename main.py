603de1ea7d23b7c66c3af245f70efa13f0501188
김 현우 <a01046807116@gmail.com>
2021-05-01 22:08:05 +0900

    asdf

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ async def change_price():
     if len(data) < 100:
         cur.execute("INSERT INTO graph_data VALUES (%s, %s, %s, %s, %s, %s, %s)",(data[-1][0]+1, bot.n_btc, bot.n_eth, bot.n_ltc, bot.n_dot, bot.n_ada, bot.n_doge))
     else:
+        cur.execute("INSERT INTO graph_data VALUES (%s, %s, %s, %s, %s, %s, %s)",(data[0][0]+1, bot.n_btc, bot.n_eth, bot.n_ltc, bot.n_dot, bot.n_ada, bot.n_doge))
         cur.execute("DELETE FROM graph_data WHERE id = %s",(data[0][0],))
-        cur.execute("INSERT INTO graph_data VALUES (%s, %s, %s, %s, %s, %s, %s)",(data[-1][0]+1, bot.n_btc, bot.n_eth, bot.n_ltc, bot.n_dot, bot.n_ada, bot.n_doge))
     conn.commit()
     cur.execute("SELECT * FROM graph_data")
     data = cur.fetchall()
