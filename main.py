638a59f851f37982588856f7b66863ef5fe2f13e
김 현우 <a01046807116@gmail.com>
2021-05-02 13:16:35 +0900

    asdf

--- main.py
+++ main.py
@@ -111,12 +111,12 @@ async def change_price():
     cur = conn.cursor()
     cur.execute("SELECT * FROM graph_data")
     data = cur.fetchall()
-    if data[-1][0] >= 100:
+    if data[0][0] >= 100:
         cur.execute("INSERT INTO graph_data VALUES (%s, %s, %s, %s, %s, %s, %s)",(1, bot.n_btc, bot.n_eth, bot.n_ltc, bot.n_dot, bot.n_ada, bot.n_doge))
-        cur.execute("DELETE FROM graph_data WHERE id = %s",(data[0][0],))
+        cur.execute("DELETE FROM graph_data WHERE id = %s",(data[-1][0],))
     else:
-        cur.execute("INSERT INTO graph_data VALUES (%s, %s, %s, %s, %s, %s, %s)",(data[-1][0]+1, bot.n_btc, bot.n_eth, bot.n_ltc, bot.n_dot, bot.n_ada, bot.n_doge))
-        cur.execute("DELETE FROM graph_data WHERE id = %s",(data[0][0],))
+        cur.execute("INSERT INTO graph_data VALUES (%s, %s, %s, %s, %s, %s, %s)",(data[0][0]+1, bot.n_btc, bot.n_eth, bot.n_ltc, bot.n_dot, bot.n_ada, bot.n_doge))
+        cur.execute("DELETE FROM graph_data WHERE id = %s",(data[-1][0],))
     conn.commit()
     cur.execute("SELECT * FROM graph_data")
     data = cur.fetchall()
