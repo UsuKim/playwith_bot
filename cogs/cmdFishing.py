a0357ad9db10cbf6728f1e423804d63828af4144
김 현우 <a01046807116@gmail.com>
2021-05-16 21:32:15 +0900

    asdf

--- cogs/cmdFishing.py
+++ cogs/cmdFishing.py
@@ -10,7 +10,7 @@ class cmdFishing(commands.Cog):
     async def cmdFishing(self, ctx, *args):
         if len(args) != 0:
             if args[0] == '상점':
-                cmdShop(ctx, '낚시')
+                cmdShop(self.bot).cmdShop(ctx, '낚시')
         embed=discord.Embed(title='미끼를 선택해 주세요.',color=0x8be653)
         await ctx.send(embed=embed)
 
