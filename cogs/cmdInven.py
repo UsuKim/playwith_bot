f91e72c2a748edcc8ebb8a1dada7bf57d4cc701b
김 현우 <a01046807116@gmail.com>
2021-05-16 20:53:16 +0900

    asdf

--- cogs/cmdInven.py
+++ cogs/cmdInven.py
@@ -9,9 +9,11 @@ class cmdInven(commands.Cog):
     async def cmdInven(self, ctx):
         self.bot.cur.execute("SELECT * FROM user_data WHERE id = %s", (str(ctx.author.id),))
         data = self.bot.cur.fetchone()
-        fs = {1:'하급 낚싯대'}
+        fs = {1:'하급 낚싯대',2:'중급 낚싯대',3:'상급 낚싯대'}
+        fs_e = {1:'<:low_fishinglod:842314659619930113>'}
         description = ''
-        description += f'낚싯대: {fs[data[22]]}({data[20]})'
+        description += f'{fs_e[data[22]]} {fs[data[22]]}({data[20]})'
+        description += f'<:normal_worm:842316324838572062> 일반 미끼: {data[21]}'
         embed=discord.Embed(title='인벤토리',description=description,color=0x8be653)
         await ctx.send(embed=embed)
 
